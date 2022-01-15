a824d386bb03a780c47168935052f1534bf99c42
Mikhail Menshikov <ixth@yandex-team.ru>
2022-01-15 12:42:31 +0300

    fix downloadImage

--- app.js
+++ app.js
@@ -139,18 +139,13 @@ function findDirectory(name) {
 }
 
 async function downloadImage(url, image_path) {
-    axios({
-        url,
-        responseType: 'stream',
-    }).then(
-        response =>
-            new Promise((resolve, reject) => {
-                response.data
-                    .pipe(fs.createWriteStream(image_path))
-                    .on('finish', () => resolve())
-                    .on('error', e => reject(e));
-            }),
-    );
+    return axios({url, responseType: 'stream'})
+        .then((response) => new Promise((resolve, reject) => {
+            response.data
+                .pipe(fs.createWriteStream(image_path))
+                .on('finish', resolve)
+                .on('error', reject);
+        }));
 }
 
 function createHeaders() {
