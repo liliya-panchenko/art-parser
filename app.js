4dd4d9bb8285f52752cfddb8240da89f563a9b84
Mikhail Menshikov <ixth@yandex-team.ru>
2022-01-15 12:42:13 +0300

    use cyrillic-to-translit-js

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const curl = require('curl');
 const jsdom = require('jsdom');
 const fs = require('fs');
 const axios = require('axios');
+const cyrillicToTranslit = require('cyrillic-to-translit-js');
+
+const {transform} = cyrillicToTranslit();
 
 const {JSDOM} = jsdom;
 const domain = 'https://collection.artsacademymuseum.org';
@@ -99,7 +102,7 @@ async function createRecord(data, id) {
         });
     }
 
-    let dir = findDirectory(trans(result.author || 'unsorted', false));
+    let dir = findDirectory(transform(result.author || 'unsorted'));
     let filenameArr = data.image.split('/');
     let filename = dir + '/' + filenameArr[filenameArr.length-1];
 
@@ -168,20 +171,3 @@ function updateLog(id) {
         console.log('Log updated');
     });
 }
-
-trans = (
-    function() {
-        let
-            rus = "щ   ш  ч  ц  ю  я  ё  ж  ъ  ы  э  а б в г д е з и й к л м н о п р с т у ф х ь".split(/ +/g),
-            eng = "shh sh ch cz yu ya yo zh `` y' e` a b v g d e z i j k l m n o p r s t u f x `".split(/ +/g)
-        ;
-        return function(text, engToRus) {
-            let x;
-            for(x = 0; x < rus.length; x++) {
-                text = text.split(engToRus ? eng[x] : rus[x]).join(engToRus ? rus[x] : eng[x]);
-                text = text.split(engToRus ? eng[x].toUpperCase() : rus[x].toUpperCase()).join(engToRus ? rus[x].toUpperCase() : eng[x].toUpperCase());
-            }
-            return text;
-        }
-    }
-)();
